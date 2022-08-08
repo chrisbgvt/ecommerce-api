6ec6fb486b6c9c51b0d818f96dead999d3a62f81
chrisbgvt <kristiqn.georgiev.bg@gmail.com>
2022-08-09 01:37:33 +0300

    add http.ok in cors

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -3,5 +3,9 @@ module.exports = () => (req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
     res.setHeader('Access-Control-Allow-Headers', '*');
 
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
     next();
 };
