9a55571021ad9d2cc81ce1068c1e507a3d468829
chrisbgvt <kristiqn.georgiev.bg@gmail.com>
2022-09-22 18:01:44 +0300

    host db

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3001;
-exports.DB_QUERY_STRING = 'mongodb://localhost:27017/ecommerceDb';
+exports.DB_QUERY_STRING = 'mongodb+srv://chris:Kg123456@cluster0.lt2ycvr.mongodb.net/ecommerceDb';
 exports.SALT_ROUNDS = 10;
 exports.SECRET = '589bynu5u942587458n';
